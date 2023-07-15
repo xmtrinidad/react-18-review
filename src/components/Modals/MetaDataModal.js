9ff49df1c0352dfce80f8031f430815e5d869ee8
Xavier <xmtrinidad@gmail.com>
2023-07-15 11:54:26 -0700

    added modal styles

--- src/components/Modals/MetaDataModal.js
+++ src/components/Modals/MetaDataModal.js
@@ -2,10 +2,19 @@ import './MetaDataModal.css'
 
 const MetaDataModal = () => {
   return (
-    <dialog open>
-      <p>Greetings, one and all!</p>
+    <dialog class="MetaDataModal" open>
       <form method="dialog">
-        <button>OK</button>
+        <div className="input-container">
+          <label htmlFor="add_tag">Add Tag:</label>
+          <input type="text" id="add_tag" name="add_tag" />
+          <button>ADD</button>
+        </div>
+        <div className="input-container">
+          <label htmlFor="gpt_link">ChatGPT Link:</label>
+          <input type="text" id="gpt_link" />
+        </div>
+        <button>CANCEL</button>
+        <button>SAVE</button>
       </form>
     </dialog>
   );
