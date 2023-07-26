bbee2e1c181939278ee48231f8ec1a67c323494c
Xavier <xmtrinidad@gmail.com>
2023-07-25 21:32:27 -0700

    checking a build thing

--- src/App.js
+++ src/App.js
@@ -1,48 +1,9 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header/Header';
 import PromptBox from './components/PromptBox/PromptBox';
 import PromptInput from './components/PromptInput/PromptInput';
 import MetaDataModal from './components/Modals/MetaDataModal';
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       prompts: [
-//         { id: 1, text: 'Some prompt text' },
-//         { id: 2, text: 'Another prompt here'}
-//       ]
-//     }
-//   }
-
-//   render() {
-
-//     const addPromptHandler = async () => {
-//       const newPrompt = { id: 3, text: 'A new prompt' };
-//       const prompts = [...this.state.prompts];
-//       prompts.push(newPrompt);
-//       await this.setState({ prompts });
-
-//       console.log(this.state.prompts);
-//     }
-
-//     return (
-//       <div className="App">
-//         <Header></Header>
-//         <main>
-//           <h2>Latest GPT Prompts</h2>
-//           <div className="prompts">
-//             <PromptBox prompt={this.state.prompts[0]}></PromptBox>
-//           </div>
-//           <div className="add-prompt-container">
-//             <button onClick={addPromptHandler}>Add Prompt</button>
-//           </div>
-//         </main>
-//       </div>
-//     );
-//   }
-// }
 
 const App = () => {
   const [prompts, setPrompts] = useState(
@@ -64,10 +25,6 @@ const App = () => {
         return [...prevState, newPrompt];
       });
     }
-
-    // useEffect(() => {
-    //   console.log(prompts);
-    // }, [prompts]);
 
     return (
       <div className="App">
